a00802856b892c91ff033a499b30238f2312256b
Iamouma <stephenouma452@gmail.com>
2024-05-23 12:43:04 +0300

    Worked on about.html and about.css file

---
+++ assets/app.js
@@ -0,0 +1,154 @@
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled',$(this).scrollTop() > 50);
+});
+
+// contact section
+$('#form').submit(function(e){
+    e.preventDefault();
+
+     const nameInput = $('#name').val();
+     const emailInput = $('#email').val();
+
+     if(nameInput && emailInput){
+        //  remove alert
+        clearALert();
+        //  create alert
+        const p = document.createElement('p');
+        // add class
+        p.className = 'alert';
+        // append text
+        p.appendChild(document.createTextNode(`Thank you ${nameInput} for reaching out to us, we will get back to you as soon as possible.`))
+        // get parent
+        const searchContainer = document.querySelector('.searchContainer');
+        // search box
+        const search = document.querySelector('.search');
+        // insert text
+        searchContainer.insertBefore(p,search);
+     }else{
+         alert('Please fill in all the required fields');
+     }
+    //  clear input fields after submit
+     $('#name').val('');
+     $('#email').val('');
+
+     setTimeout(()=>{
+         clearALert();
+     },4000);
+});
+// end of contact section
+function clearALert(){
+    const currentAlert = document.querySelector('.alert');
+    if(currentAlert){
+        currentAlert.remove();
+    }
+}
+const productItems = document.querySelector('.productItems');
+
+function displayProducts(products){
+    productItems.innerHTML = "";
+    products.forEach((product)=>{
+        productItems.innerHTML += `
+                <div class="col-md-3">
+                <div class="card mb-3">
+                <a href="more.html">
+                <img
+                    src="${product.imgSrc}"
+                    class="card-img-top img-fluid"
+                    alt="..."
+                />
+                </a>
+                <div class="card-body">
+                    <div class="content">
+                    <h5 class="card-title price">ksh${product.price}</h5>
+                    <a href="details.html" target="_blank">
+                    <img class="img-fluid" src="https://img.icons8.com/ios-filled/50/fa314a/plus.png"/>
+                    </a>
+                    </div>
+                    <div class="info">
+                    <p class="card-text">${product.rooms}Bdrm</p>
+                    <p class="card-text">${product.size}sqm</p>
+                    <p class="card-text">Gated</p>
+                    </div>
+                    <div class="location">
+                    <div>
+                        <span>Estate</span><br />
+                        <span>${product.location}</span>
+                    </div>
+                    <div>
+                        <span>AGent</span><br />
+                        <span>${product.agent}</span>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        `
+    })
+}
+            displayProducts(products);
+            $('#location').on('change',function(){
+
+                var selectedLocation = this.value;
+                
+                if(selectedLocation == 1){
+                    var location = products.filter(product => product.location == 'Lower Kabete');
+                }else if(selectedLocation == 2){
+                    var location = products.filter(product => product.location == 'Westlands');
+                }else if(selectedLocation == 3){
+                    var location = products.filter(product => product.location == 'Kilimani');
+                }else if(selectedLocation == 4){
+                    var location = products.filter(product => product.location == 'Lavington');
+                }else if(selectedLocation == 5){
+                    var location = products.filter(product => product.location == 'Kileleshwa');
+                }else if(selectedLocation == 6){
+                    var location = products.filter(product => product.location == 'Karen');
+                }else if(selectedLocation == 7){
+                    var location = products.filter(product => product.location == 'Runda');
+                }else if(selectedLocation == 8){
+                    var location = products.filter(product => product.location == 'Limuru Road');
+                }else if(selectedLocation == 9){
+                    var location = products.filter(product => product.location == 'Muthaiga');
+                }
+                displayProducts(location);
+            })
+            $('#price').on('change',function(){
+                var selectedPrice = this.value;
+
+                if(selectedPrice == 1){
+                    var price = products.filter(product => product.price <= 10000000);
+                }else if(selectedPrice == 2){
+                    var price = products.filter(product => product.price  > 11000000 && product.price <=20000000);
+                }else if(selectedPrice == 3){
+                    var price = products.filter(product => product.price  > 21000000 );
+                }
+                console.log(price);
+                displayProducts(price);
+            })
+
+            $('#rooms').on('change',function(){
+                var selectedRooms = this.value;
+
+                if(selectedRooms == 1){
+                    var room = products.filter(product => product.rooms  <= 3 );
+                }else if(selectedRooms == 2){
+                    var room = products.filter(product => product.rooms  > 3 && product.rooms <= 6 );
+                }else if(selectedRooms == 3){
+                    var room = products.filter(product => product.rooms  > 6 && product.rooms <= 10 );
+                }
+                displayProducts(room);
+            })
+
+            $('#size').on('change',function(){
+                var selectedSize = this.value;
+
+                if(selectedSize == 1){
+                    var size = products.filter(product => product.size <= 100);
+               }else if(selectedSize == 2){
+                    var size = products.filter(product => product.size >= 111 && product.size <= 150);
+                }else if(selectedSize == 3){
+                    var size = products.filter(product => product.size >= 151 && product.size <= 200);
+                }else if(selectedSize = 4){
+                    var size = products.filter(product => product.size >= 201);
+                }
+                displayProducts(size);
+            })
---
+++ assets/products.js
@@ -0,0 +1,217 @@
+const products = [
+    {
+        id: 0,
+        imgSrc : '/Images/image2.jpeg',
+        location: 'Lower Kabete',
+        price: 13000000,
+        rooms: 3,
+        size: 120
+    },
+    {
+        id: 1,
+        imgSrc : '/Images/image3.jpeg',
+        location: 'Kiambu Road',
+        price: 1100000,
+        rooms: 2,
+        agent: 'Marcos',
+        size: 100
+
+    },
+    {
+        id: 2,
+        imgSrc : '/Images/image4.jpeg',
+        location: 'Kilimani',
+        price: 4400000,
+        rooms: 4,
+        agent: 'George',
+        size: 150
+    },
+    {
+        id: 3,
+        imgSrc : '/Images/image5.jpg',
+        location: 'Westlands',
+        price: 1200000,
+        rooms: 3 ,
+        agent: 'Erick',
+        size: 110
+    },
+    {
+        id: 4,
+        imgSrc : '/Images/image6.jpg',
+        location: 'Karen',
+        price: 60000000,
+        rooms: 7,
+        agent: 'Vanessa',
+        size: 160
+    },
+    {
+        id: 5,
+        imgSrc : '/Images/image7.jpg',
+        location: 'Runda',
+        price: 7300000,
+        rooms: 5,
+        agent: 'Marcos',
+        size: 180
+    },
+    {
+        id: 6,
+        imgSrc : '/Images/image8.jpg',
+        location: 'Lavington',
+        price: 84000000,
+        rooms: 6,
+        agent: 'Geroge',
+        size: 180
+    },
+    {
+        id: 7,
+        imgSrc : '/Images/image9.jpg',
+        location: 'Kitengela',
+        price: 2200000,
+        rooms: 3,
+        agent: 'Erick',
+        size: 115
+    },
+    {
+        id: 8,
+        imgSrc : '/Images/image10.jpg',
+        location: 'Runda',
+        price: 7700000,
+        rooms: 7,
+        agent: 'Vanessa',
+        size: 145,
+    },
+    {
+        id: 9,
+        imgSrc : '/Images/image11.jpg',
+        location: 'Karen',
+        price: 6100000,
+        rooms: 3,
+        agent: 'Marcos',
+        size: 148
+    },
+    {
+        id: 10,
+        imgSrc : '/Images/image12.jpg',
+        location: 'Kiambu Road',
+        price: 5500000,
+        rooms: 2,
+        agent: 'George',
+        size: 120
+    },
+    {
+        id: 11,
+        imgSrc : '/Images/image14.jpg',
+        location: 'Kilimani',
+        price: 44000000,
+        rooms: 4,
+        agent: 'Erick',
+        size: 124
+    },
+    {
+        id: 12,
+        imgSrc : '/Images/image15.jpg',
+        location: 'Spring Valley',
+        price: 12000000,
+        rooms: 5,
+        agent: 'Vanessa',
+        size: 200
+    },
+    {
+        id: 13,
+        imgSrc : '/Images/image16.jpg',
+        location: 'Muthaiga',
+        price: 15000000,
+        rooms: 6,
+        agent: 'Marcos',
+        size: 220
+    },
+    {
+        id: 14,
+        imgSrc : '/images/image17.jpeg',
+        location: 'Gigiri',
+        price: 19000000,
+        rooms: 7,
+        agent: 'George',
+        size: 130
+    },
+    {
+        id: 15,
+        imgSrc : '/Images/image18.jpg',
+        location: 'Kilimani',
+        price: 25000000,
+        rooms: 6,
+        agent: 'Erick',
+        size: 232
+    },
+    {
+        id: 16,
+        imgSrc : '/Images/image19.jpg',
+        location: 'Muthaiga',
+        price: 25000000,
+        rooms: 4,
+        agent: 'Erick',
+        size: 190
+    },
+    {
+        id: 17,
+        imgSrc : '/Images/7.jpg',
+        location: 'Muthaiga',
+        price: 43000000,
+        rooms: 6,
+        agent: 'Vanessa',
+        size: 127
+    },
+    {
+        id: 18,
+        imgSrc : '/Images/11.jpg',
+        location: 'Spring Valley',
+        price: 29000000,
+        rooms: 7,
+        agent: 'Marcos',
+        size: 111
+    },
+    {
+        id: 19,
+        imgSrc : '/Images/9.jpg',
+        location: 'Spring Valley',
+        price: 50000000,
+        rooms: 3,
+        agent: 'George',
+        size: 176
+    },
+    {
+        id: 20,
+        imgSrc : '/Images/9.jpg',
+        location: 'Lavington',
+        price: 12000000,
+        rooms: 9,
+        agent: 'Vanessa',
+        size: 130
+    },
+    {
+        id: 21,
+        imgSrc : '/Images/9.jpg',
+        location: 'Westlands',
+        price: 33000000,
+        rooms: 4,
+        size: 175
+    },
+    {
+        id: 22,
+        imgSrc : '/Images/9.jpg',
+        location: 'Karen',
+        price: 34000000,
+        rooms: 5,
+        agent: 'Erick',
+        size: 177
+    },
+    {
+        id: 23,
+        imgSrc : '/Images/9.jpg',
+        location: 'Kiambu Road',
+        price: 22000000,
+        rooms: 4,
+        agent: 'Erick',
+        size: 141
+    },
+]
